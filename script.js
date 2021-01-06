66063ef77d360d14072bc3442d869a44e698c644
David <david.grech26@gmail.com>
2021-01-06 10:40:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const todos = document.getElementById('todos');
+const todosUl = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos){
+    todos.forEach(todo => {
+        addTodo(todo);
+    })
+}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     addTodo();
-
 });
 
-function addTodo(){
-    const todoText = input.value;
+function addTodo(todo){
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo.text;
+    }
 
     if(todoText){
         const todoEl = document.createElement('li');
+
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed');
+        }
         todoEl.innerText = todoText;
 
         todoEl.addEventListener('click', () => {
-            todoEl.classList.toggle("completed");
+            todoEl.classList.toggle('completed');
 
             updateLS();
         });
@@ -30,7 +45,7 @@ function addTodo(){
             updateLS();
         });
 
-        todos.appendChild(todoEl);
+        todosUl.appendChild(todoEl);
 
         input.value = "";
 
