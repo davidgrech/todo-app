bee4e6b21d7e88274e79c2d8f1931ad3af6892fb
David <david.grech26@gmail.com>
2021-01-06 10:29:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const todos = document.getElementById('todos');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
+    addTodo();
+
+});
+
+function addTodo(){
     const todoText = input.value;
 
     if(todoText){
@@ -13,16 +18,38 @@ form.addEventListener('submit', (e) => {
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle("completed");
+
+            updateLS();
         });
 
-        todoEl.addEventListener("contextmenu", (e) => {
+        todoEl.addEventListener('contextmenu', (e) => {
             e.preventDefault();
 
             todoEl.remove();
+
+            updateLS();
         });
 
         todos.appendChild(todoEl);
 
         input.value = "";
+
+        updateLS();
     }
-});
+
+}
+
+function updateLS(){
+    const todosEl = document.querySelectorAll('li');
+
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains('completed'),
+        })
+    });
+
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
